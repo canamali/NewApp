b8c65656562acee8d497ee074e26b7ad1a1bc0dd
canamali <anamaliachristopher@gmail.com>
2022-01-03 08:43:07 -0700

    Name change

--- ios/DialogFrontEndTests/DialogFrontEndTests.m
+++ ios/DialogFrontEndTests/DialogFrontEndTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DialogFrontEndTests : XCTestCase
+@interface MobileFrontEndTests : XCTestCase
 
 @end
 
-@implementation DialogFrontEndTests
+@implementation MobileFrontEndTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
